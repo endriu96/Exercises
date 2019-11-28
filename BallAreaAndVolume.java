c91a4344d4289e4b6222cbd0de7dca6d312f4bea
endriu96 <endriu96@gmail.com>
2019-11-28 21:54:40 +0100

    Update and rename Cw5.java to BallAreaAndVolume.java
    Program, który przyjmuje wartość i oblicza pole oraz objętość kuli

--- BallAreaAndVolume.java
+++ BallAreaAndVolume.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Cw5 {
+public class BallAreaAndVolume{
     public static void main(String[] args){
         System.out.println("Podaj promien kuli");
         Scanner scan = new Scanner(System.in);
@@ -10,13 +10,12 @@ public class Cw5 {
         double sphereField=4*Math.PI*Math.pow(radius,powField);
         if (radius<=0){
             System.out.println("Promien nie może być mniejszy badz rowny zero ");
-            //1.Co zrobić żeby program nie pokazał wyniku?
+         
         }
         double volumeOfTheBall=(4/3)*Math.PI*Math.pow(radius,powVolume);
         System.out.println("Pole kuli wynosi "+ sphereField);
         System.out.println("Objetosc kuli wynosi "+ volumeOfTheBall);
     }
-//2.Dlaczego w wyniku jest tak dużo miejsc po przecinku?
-//3.Dlaczego program nie przyjmuje liczb zmiennoprzecinkowych?
+
 
 }
